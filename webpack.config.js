d5ff03ecfc2b77e6fb1577c2361d4784df36d301
Karlen Manaseryan <kmanaseryan@gmail.com>
2020-08-31 14:59:03 +0400

    [webpack, module][xs]: build configurations - refs #1

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
 const path = require('path')
 const assetPath = './lib'
 
-module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  context: path.resolve(__dirname),
-  entry: {
-    index: `${assetPath}/index.js`,
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    libraryTarget: 'var',
-    library: 'frictionlessCkanMapper', // The variable name to access the library
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  node: { fs: 'empty' },
+const createConfig = (target) => {
+  return {
+    mode: 'production',
+    devtool: 'source-map',
+    context: path.resolve(__dirname),
+    entry: {
+      index: `${assetPath}/index.js`,
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: `frictionless-ckan-mapper.${target}.js`,
+      libraryTarget: target,
+      library: 'frictionlessCkanMapper',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    node: { fs: 'empty' },
+  }
 }
+
+module.exports = [
+  createConfig('var'),
+  createConfig('commonjs2'),
+  createConfig('amd'),
+  createConfig('umd'),
+]
