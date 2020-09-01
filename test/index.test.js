145fb75f177d991c3fd7286954f6d9ceef032c62
Karlen Manaseryan <kmanaseryan@gmail.com>
2020-09-01 17:15:13 +0400

    [test][s]: Write tests for ckan->frictionless covnersion - refs #5

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,5 @@
 // Test frictionless -> ckan
 require('./frictionless-to-ckan.resource.test')
 require('./frictionless-to-ckan.package.test')
+require('./ckan-to-frictionless.resource.test')
+require('./ckan-to-frictionless.package.test')
