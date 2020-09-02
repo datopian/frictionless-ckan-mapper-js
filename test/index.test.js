04b4c8aadb3846cf7f152ae2095eee7444cebd3a
Karlen Manaseryan <kmanaseryan@gmail.com>
2020-09-02 14:16:57 +0400

    [test,add][s]: Write full roundtrip test for conversion - #5

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,10 @@
 // Test frictionless -> ckan
 require('./frictionless-to-ckan.resource.test')
 require('./frictionless-to-ckan.package.test')
+
+// Test ckan -> frictionless
 require('./ckan-to-frictionless.resource.test')
 require('./ckan-to-frictionless.package.test')
+
+// Test ckan -> frictionless -> ckan -> frictionless -> ckan
+require('./roundtrip.test')
